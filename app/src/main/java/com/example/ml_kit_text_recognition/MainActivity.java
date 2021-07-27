124922813080fed0d0d5b74a8f396f22a31bcf72
jorgmoli97 <jorgmoli97@users.noreply.github.com>
2021-07-27 12:34:17 -0500

    first push
    first push

---
+++ app/src/main/java/com/example/ml_kit_text_recognition/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ml_kit_text_recognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
